a5185faee35fad6854773d9df67e05616f481f05
Hassan Juma <okothhassanjuma@gmail.com>
2020-09-06 22:26:22 +0300

    add location scripts

--- JS/main.js
+++ JS/main.js
@@ -1,106 +1,188 @@
 $(document).ready(function () {
-    $("#order1").submit(function (event) {
-        var typeInput = $("input#type").val();
-        var sizeInput = $("input#size").val();
-        var numberInput = $("input#number").val();
-        var crustInput = $("input#crust").val();
-        var toppingsInput = $("input#toppings").val();
-        var ordertypeInput = $("input#ordertype").val();
-        $(".type").text(person1Input);
-        $(".size").text(person2Input);
-        $(".number").text(animalInput);
-        $(".crust").text(exclamationInput);
-        $(".toppings").text(verbInput);
-        $(".ordertype").text(nounInput);
+    function Pizza(type, size, topping, crust) {
+        this.type = type;
+        this.size = size;
+        this.topping = topping;
+        this.crust = crust;
+    }
 
-        $("#outcome").show();
+    Pizza.prototype.getPizzaPrice = function () {
+        return this.getCrustPrice() + this.getToppingPrice() + this.getTypePrice();
+    };
 
-        event.preventDefault();
+    Pizza.prototype.getToppingPrice = function () {
+        if (this.size === "large") {
+            if (this.topping === "pepperoni") {
+                return 150;
+            } else if (this.topping === "mushroom") {
+                return 120;
+            } else {
+                return 180;
+            }
+        } else if (this.size === "medium") {
+            if (this.topping === "pepperoni") {
+                return 100;
+            } else if (this.topping === "mushroom") {
+                return 80;
+            } else {
+                return 140;
+            }
+        } else {
+            if (this.topping === "pepperoni") {
+                return 80;
+            } else if (this.topping === "mushroom") {
+                return 50;
+            } else {
+                return 100;
+            }
+        }
+    };
+
+    Pizza.prototype.getCrustPrice = function () {
+        if (this.crust === "thick") {
+            return 100;
+        } else {
+            return 0;
+        }
+    };
+
+    Pizza.prototype.getTypePrice = function () {
+        if (this.size === "large") {
+            if (this.type === "chicken tikka") {
+                return 1200;
+            } else if (this.type === "peri peri") {
+                return 1300;
+            } else {
+                return 1100;
+            }
+        } else if (this.size === "medium") {
+            if (this.type === "chicken tikka") {
+                return 850;
+            } else if (this.type === "peri peri") {
+                return 950;
+            } else {
+                return 750;
+            }
+        } else {
+            if (this.type === "chicken tikka") {
+                return 600;
+            } else if (this.type === "peri peri") {
+                return 650;
+            } else {
+                return 550;
+            }
+        }
+    };
+
+    var customerName = "";
+
+    var totalCost = 0;
+
+    var pizzasOrdered = [];
+
+    var estate = "";
+    var houseNumber = "";
+
+    $("#pizza-form").submit(function (e) {
+        e.preventDefault();
+        var typeSelected = $("#type").val();
+        var sizeSelected = $("#size").val();
+        var toppingSelected = $("#topping").val();
+        var crustSelected = $("#crust").val();
+
+        var newPizza = new Pizza(
+            typeSelected,
+            sizeSelected,
+            toppingSelected,
+            crustSelected
+        );
+
+        pizzasOrdered.push(newPizza);
+        $("#type").val("");
+        $("#size").val("");
+        $("#topping").val("");
+        $("#crust").val("");
+
+        totalCost = 0;
+
+        for (let i = 0; i < pizzasOrdered.length; i++) {
+            totalCost += pizzasOrdered[i].getPizzaPrice();
+        }
+
+        $("#order-summary").append(
+            "<tr>" +
+            '<th scope="row">' +
+            newPizza.type +
+            " (" +
+            newPizza.size +
+            ") - " +
+            newPizza.getTypePrice() +
+            "</th>" +
+            "<td>" +
+            newPizza.topping +
+            " - " +
+            newPizza.getToppingPrice() +
+            "</td>" +
+            "<td>" +
+            newPizza.crust +
+            " - " +
+            newPizza.getCrustPrice() +
+            "</td>" +
+            "<td>" +
+            newPizza.getPizzaPrice() +
+            "</td>" +
+            "</tr>"
+        );
+
+        if (pizzasOrdered.length > 0) {
+            $("#form-title").empty();
+            $("#form-title").append("Add Another Order");
+        }
+
+        $("#total-amount").fadeIn();
+        $("#checkout").fadeIn();
+        $("#orders-div").fadeIn();
+
+        $("#total-amount").empty();
+        $("#total-amount").append(totalCost);
+        $(".total-amount").show();
+    });
+
+    $("#checkout").click(function () {
+        $(".checkout-options").show();
+    });
+
+    $("#checkout-form").submit(function (e) {
+        e.preventDefault();
+        var name = $("#name").val();
+        var deliveryOption = $("#delivery-option").val();
+        customerName = name;
+        console.log(name);
+        console.log(deliveryOption);
+        $("#name").val("");
+        $("#delivery-option").val("");
+        $(".checkout-options").hide();
+        if (deliveryOption === "deliver") {
+            $(".location").show();
+            $(".delivery-cost").show();
+            $("#delivery-amount").append(200);
+            totalCost += 200;
+            $("#total-amount").empty();
+            $("#total-amount").append(totalCost);
+        } else {
+            alert(customerName + ": Your total bill is Ksh. " + totalCost + ". Your order will be ready for collection in the next 2 hours");
+        }
+    });
+
+    $("#location-form").submit(function (e) {
+        e.preventDefault();
+        var estateEntered = $("#estate").val();
+        var houseNumberEntered = $("#house-number").val();
+        estate = estateEntered;
+        houseNumber = houseNumberEntered;
+        console.log(estate);
+        console.log(houseNumber);
+        $(".location").hide();
+        alert(customerName + ": Your total bill is : Ksh. " + totalCost + ". Your order will be delivered to " + estate + " Estate, House Number : " + houseNumber + " in 30 minutes ime.Thanks for doing business.");
     });
 });
-$(document).ready(function () {
-            $(".btn").click(function () {
-                $("#outcome").show(1000);
-            })
-            $(document).ready(function () {
-                let all0rders = []
-
-                $("#order1").submit(function (event) {
-                    var location = prompt("Where do you like yor order to be delivered?")
-                    var order = ["type", "size", "number", "crust", "toppings", "ordertype"];
-
-                    // let location = $("location").val()
-                    let type = $("#type").val()
-                    let size = $("#size").val()
-                    let number = $("#number").val()
-                    let crust = $("#crust").val()
-                    let toppings = $("#toppings").val()
-                    let ordertype = $("#ordertype").val()
-
-                    let newOrder = new Order(type, size, number, crust, toppings, ordertype);
-                    allOrders.push(newOrder);
-
-
-
-                    blanks.forEach(function (blank) {
-                        var userInput = $("input." + blank).val();
-                        $("." + blank).text(userInput).val();
-                    });
-
-                    $("#outcome").show();
-
-                    event.preventDefault();
-                });
-            });
-
-            function Order(type, size, number, crust, toppings, ordertype) {
-                this.type = type
-                this.size = size
-                this.number = number
-                this.crust = crust
-                this.toppings = toppings
-                this.ordertype = ordertype
-
-                //     order.prototype.size() {
-                //         return size
-                //     }
-                //     order.prototype.size() {
-                //         return size
-                //     }
-                //     order.prototype.size() {
-                //         return size
-                //     }
-                //     order.prototype.topping() {
-                //         return topping
-                //     }
-                //     order.prototype.topping() {
-                //         return topping
-                //     }
-                //     order.prototype.topping() {
-                //         return topping
-                //     }
-                //     order.prototype.crust() {
-                //         return crust
-                //     }
-                //     order.prototype.crust() {
-                //         return crust
-                //     }
-                //     order.prototype.crust() {
-                //         return crust
-                //     }
-                //     order.prototype.ordertype() {
-                //         return ordertype
-                //     }
-                //     order.prototype.ordertype() {
-                //         return ordertype
-                //     }
-                //     order.prototype.number() {
-                //         return number
-                //     }
-                //     let newOrder = new Order(type, size, number, crust, toppings, ordertype);
-                //     allOrders.push(newOrder);
-                //     newOrder.size()
-
-
-
-                // }
